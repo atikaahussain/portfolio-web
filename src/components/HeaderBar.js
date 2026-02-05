8a279de26b0ab18d2964a4d3a77974cac0bd2848
Atika Hussain <intjradiant@gmail.com>
2026-02-05 23:44:51 +0500

    .

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -10,17 +10,41 @@ const HeaderBar = () => {
           href="mailto:atikahussain248@gmail.com"
           className="fa fa-envelope"
           style={{ fontSize: '18px' }}
+          aria-label="Send email to Atika Hussain"
         ></a>
-        <a href="https://www.linkedin.com/in/atikaahussain248">
-          <i className="fa-brands fa-linkedin" style={{ fontSize: '18px' }}></i>
-        </a>
-        <a href="https://github.com/atikaahussain">
-          <i className="fa-brands fa-github" style={{ fontSize: '18px' }}></i>
-        </a>
-        <a href="https://medium.com/@atikaahussain">
-          <i className="fa fa-pencil" style={{ fontSize: '18px' }}></i>
+
+        <a
+          href="https://www.linkedin.com/in/atikaahussain248"
+          aria-label="Atika Hussain LinkedIn profile"
+        >
+          <i
+            className="fa-brands fa-linkedin"
+            style={{ fontSize: '18px' }}
+            aria-hidden="true"
+          ></i>
         </a>
 
+        <a
+          href="https://github.com/atikaahussain"
+          aria-label="Atika Hussain GitHub profile"
+        >
+          <i
+            className="fa-brands fa-github"
+            style={{ fontSize: '18px' }}
+            aria-hidden="true"
+          ></i>
+        </a>
+
+        <a
+          href="https://medium.com/@atikaahussain"
+          aria-label="Atika Hussain Medium articles"
+        >
+          <i
+            className="fa fa-pencil"
+            style={{ fontSize: '18px' }}
+            aria-hidden="true"
+          ></i>
+        </a>
       </div>
     </div>
   );
